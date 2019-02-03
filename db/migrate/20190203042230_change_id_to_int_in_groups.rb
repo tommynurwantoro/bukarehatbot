1bfbf1f99a566214926cf4a54c28e6ab26392f52
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2019-02-04 00:04:31 +0700

    Change group id to bigint

--- db/migrate/20190203042230_change_id_to_int_in_groups.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIdToIntInGroups < ActiveRecord::Migration[5.2]
-  def change
-    change_column :groups, :id, :int
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_02_03_042230) do
 
-  create_table "groups", id: :integer, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "groups", id: :bigint, default: nil, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
   end
 
