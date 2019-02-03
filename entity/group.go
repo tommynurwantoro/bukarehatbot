5bb322b69135529fdf9b67530a210be9a2037217
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2019-02-03 12:14:19 +0700

    Update entities

---
+++ entity/group.go
@@ -0,0 +1,7 @@
+package entity
+
+// Group _
+type Group struct {
+	ID   int64  `db:"id"`
+	Name string `db:"name"`
+}
--- entity/user.go
+++ entity/user.go
@@ -2,7 +2,11 @@ package entity
 
 // User _
 type User struct {
-	ID       uint64 `db:"id"`
-	Username string `db:"username"`
-	IsAdmin  bool   `db:"is_admin"`
+	ID        uint64 `db:"id"`
+	Username  string `db:"username"`
+	GroupID   int64  `db:"group_id"`
+	IsAdmin   bool   `db:"is_admin"`
+	Point     uint64 `db:"point"`
+	CreatedAt string `db:"created_at"`
+	UpdatedAt string `db:"updated_at"`
 }
