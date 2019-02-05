c8401a842af946627e63a39b07b8104262b59e51
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2019-02-05 17:28:18 +0700

    Using time.Now() to fill timestamp columns

--- utility/mysql/microbreak.go
+++ utility/mysql/microbreak.go
@@ -1,12 +1,14 @@
 package mysql
 
 import (
+	"time"
+
 	"github.com/bot/bukarehatbot/app"
 )
 
 // InsertOneMicrobreak _
 func InsertOneMicrobreak(groupID int64, url string, hour int, minute int) {
-	now := "2018-11-01 02:43:48"
+	now := time.Now()
 	_, err := app.MysqlClient.Exec(
 		"INSERT INTO microbreaks(group_id, url, rest_hour, rest_minute, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)",
 		groupID, url, hour, minute, now, now)
