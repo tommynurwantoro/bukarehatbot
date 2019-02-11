0ab83a65cd72cd7d555e37945fcfc60b179e5b87
tommynurwantoro <tommy.nurwantoro@gmail.com>
2019-02-11 16:45:58 +0700

    Update command string

--- entity/command.go
+++ entity/command.go
@@ -10,12 +10,11 @@ type Command struct {
 func GroupCommands() []Command {
 	return []Command{
 		{"/halo", "Cuma buat nyapa aja\n"},
-		{"/init_admin", "Set admin untuk pertama kali\n"},
-		{"/change_admin", "Ngubah admin ke member lain di dalam tim\n"},
-		{"/add_member", "Nambahin user ke bukarehat\n"},
+		{"/change_admin", "{username} Ngubah admin ke member lain di dalam tim\n"},
+		{"/add_member", "{username} Nambahin user ke bukarehat\n"},
 		{"/show_group_name", "Nunjukin nama tim kamu untuk leaderboard\n"},
-		{"/change_group_name", "Ngubah nama tim kamu untuk leaderboard\n"},
-		{"/micro", "Ngatur jam microbreak buat tim kamu\n"},
+		{"/change_group_name", "{name} Ngubah nama tim kamu untuk leaderboard\n"},
+		{"/micro", "{HH:mm} Ngatur jam microbreak buat tim kamu\n"},
 		{"/show_micros", "Nunjukin jam-jam microbreak yang udah diatur admin tim kamu\n"},
 		{"/leaderboard", "Nunjukin poin-poin yang dimiliki tiap member\n"},
 	}
